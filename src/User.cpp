55c3a6d5d3d80201cd9b3e957e404e3472455fb6
octoross-42 <codingcoeane@gmail.com>
2025-05-20 18:02:14 +0200

    User: handleLine in comments

--- src/User.cpp
+++ src/User.cpp
@@ -6,7 +6,7 @@
 /*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:55:10 by gtraiman          #+#    #+#             */
-/*   Updated: 2025/05/20 16:43:08 by octoross         ###   ########.fr       */
+/*   Updated: 2025/05/20 17:27:18 by octoross         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,64 +99,64 @@ std::string join_rest(const std::vector<std::string>& v, size_t start)
 }
 
 
-void Server::handleLine(int fd, const std::string& line)
-{
-    // On récupère l'utilisateur
-    User* u = getUser(fd);
-    if (!u)
-        return; // pas trouvé, on ignore
+// void Server::handleLine(int fd, const std::string& line)
+// {
+//     // On récupère l'utilisateur
+//     User* u = getUser(fd);
+//     if (!u)
+//         return; // pas trouvé, on ignore
 
-    // On découpe la ligne en tokens
-    std::vector<std::string> tokens = split(line, ' ');
-    if (tokens.empty())
-        return;
+//     // On découpe la ligne en tokens
+//     std::vector<std::string> tokens = split(line, ' ');
+//     if (tokens.empty())
+//         return;
 
-    // Le verbe IRC est le premier token
-    std::string cmd = tokens[0];
+//     // Le verbe IRC est le premier token
+//     std::string cmd = tokens[0];
 
-    // JOIN et PART prennent tous leurs paramètres après le verbe
-    std::vector<std::string> params(tokens.begin() + 1, tokens.end());
-    if (cmd == "JOIN")
-    {
-        if(params.empty())
-            return(write(fd,"You need a name for your channel",32), (void)cmd);
-        cmd_JOIN(u, params);
-    }
-    else if (cmd == "PART")
-    {
-        cmd_PART(u, params);
-    }
-    // PRIVMSG prend 2 paramètres explicitement : cible et message
-    else if (cmd == "PRIVMSG")
-    {
-        // On vérifie qu'on a au moins "PRIVMSG cible texte..."
-        if (tokens.size() >= 3)
-        {
-            std::string target  = tokens[1];
-            std::string message = join_rest(tokens, 2);
-            std::vector<std::string> params;
-            params.push_back(target);
-            params.push_back(message);
-            cmd_PRIVMSG(u, params);
-        }
-    }
-    else if (cmd == "TOPIC")
-    {
-        if(params.empty())
-            return(write(fd,"You need a name for your topic",30), (void)cmd);
-        std::vector<std::string> params(tokens.begin() + 1, tokens.end());
-        cmd_TOPIC(u, params);
-    }
-    else
-    {
-        // On reconstruit correctement params pour cmd_MSG
-        std::vector<std::string> fixedParams;
-        fixedParams.push_back(cmd); // l'ancien "cmd" est ici en réalité le nom du canal
-        fixedParams.insert(fixedParams.end(), params.begin(), params.end());
-        cmd_MSG(u, fixedParams);
-    }
+//     // JOIN et PART prennent tous leurs paramètres après le verbe
+//     std::vector<std::string> params(tokens.begin() + 1, tokens.end());
+//     if (cmd == "JOIN")
+//     {
+//         if(params.empty())
+//             return(write(fd,"You need a name for your channel",32), (void)cmd);
+//         cmd_JOIN(u, params);
+//     }
+//     else if (cmd == "PART")
+//     {
+//         cmd_PART(u, params);
+//     }
+//     // PRIVMSG prend 2 paramètres explicitement : cible et message
+//     else if (cmd == "PRIVMSG")
+//     {
+//         // On vérifie qu'on a au moins "PRIVMSG cible texte..."
+//         if (tokens.size() >= 3)
+//         {
+//             std::string target  = tokens[1];
+//             std::string message = join_rest(tokens, 2);
+//             std::vector<std::string> params;
+//             params.push_back(target);
+//             params.push_back(message);
+//             cmd_PRIVMSG(u, params);
+//         }
+//     }
+//     else if (cmd == "TOPIC")
+//     {
+//         if(params.empty())
+//             return(write(fd,"You need a name for your topic",30), (void)cmd);
+//         std::vector<std::string> params(tokens.begin() + 1, tokens.end());
+//         cmd_TOPIC(u, params);
+//     }
+//     else
+//     {
+//         // On reconstruit correctement params pour cmd_MSG
+//         std::vector<std::string> fixedParams;
+//         fixedParams.push_back(cmd); // l'ancien "cmd" est ici en réalité le nom du canal
+//         fixedParams.insert(fixedParams.end(), params.begin(), params.end());
+//         cmd_MSG(u, fixedParams);
+//     }
 
-}
+// }
 
 // ssize_t User::send(const std::string& msg, int flags)
 // {
