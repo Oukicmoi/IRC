9360742ab1cc170aa19c32cd794046b0d7b3641e
octoross <codingoceane@gmail.com>
2025-06-15 14:22:18 +0200

    Channel: added Channel dir to sort Channel methods

---
+++ src/Channel/Channel.cpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Channel.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 15:54:36 by gtraiman          #+#    #+#             */
+/*   Updated: 2025/06/15 14:19:12 by octoross         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "all.hpp"
+
+Channel::Channel(const std::string& name, Server &server):
+	_inviteOnly(false),
+	_topicRestricted(false),
+	_hasKey(false),
+	_userLimit(-1),
+	_creationTime(std::time(NULL)),
+	_topicSetTime(std::time(NULL)),
+	_name(name),
+	_key(""),
+	_topic(""),
+	_topicSetter(""),
+	_server(server) {}
+
+bool Channel::addMember(User* u)
+{
+    std::pair<std::set<User*>::iterator,bool> result = _members.insert(u);
+    return (result.second);
+}
+
+void Channel::removeMember(User* user)
+{
+    _members.erase(user);
+    _operators.erase(user);
+	std::map<time_t, User *>::iterator it = _membersJoinOrder.begin();
+	while (it != _membersJoinOrder.end())
+	{
+		if (it->second == user)
+		{
+			_membersJoinOrder.erase(it->first);
+			break;
+		}
+	}
+	if (_operators.empty() && !(_membersJoinOrder.empty()))
+	{
+		User *newOperator = _membersJoinOrder.begin()->second;
+		_operators.insert(newOperator);
+		broadcast(RPL_SERVER_MODE(_name,  "+o", newOperator->getNick()), user);
+	}
+	
+}
+
+
+void Channel::addOperator(User* u)
+{
+	_members.insert(u);
+	_operators.insert(u);
+	_membersJoinOrder[std::time(NULL)] = u;
+}
+void Channel::removeOperator(User* user) { _operators.erase(user); }
+
+
+std::string	Channel::getNickList() const
+{
+	std::string nickList;
+	for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
+	{
+		if (!nickList.empty())
+			nickList += " ";
+		if (isOperator(*it))
+			nickList += "@";
+		nickList += (*it)->getNick();
+	}
+	return (nickList);
+}
+
+void Channel::broadcast(const std::string& message, User* except) const
+{
+    for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
+    {
+        User* u = *it;
+        if (u == except)
+            continue;
+        // envoie le message à chaque membre (sauf « except »)
+        Server::sendToUser(u->getSocketFd(), message);
+    }
+} 
+
+
+void Channel::printOperators() const
+{
+    std::cout << "Operators on channel " << _name << " (“ operators.size() = " << _operators.size() << "):\n";
+    for (std::set<User*>::const_iterator it = _operators.begin(); it != _operators.end(); ++it)
+        std::cout << "  - " << (*it)->getNick() << "\n";
+}
+
+void Channel::printMembers() const
+{
+    std::cout << "Members on channel " << _name << " (“ _members.size() = " << _members.size() << "):\n";
+    for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
+        std::cout << "  - " << (*it)->getNick() << "\n";
+}
+
+void Channel::printInviteList() const
+{
+    std::cout << "Invite users on channel " << _name << " (“ _inviteList.size() = " << _inviteList.size() << "):\n";
+    for (std::set<User*>::const_iterator it = _inviteList.begin(); it != _inviteList.end(); ++it)
+        std::cout << "  - " << (*it)->getNick() << "\n";
+}
--- src/Channel/modes.cpp
+++ src/Channel/modes.cpp
@@ -1,169 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Channel.cpp                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: octoross <octoross@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/13 15:54:36 by gtraiman          #+#    #+#             */
-/*   Updated: 2025/06/15 03:19:40 by octoross         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "all.hpp"
-
-Channel::Channel(const std::string& name, Server &server):
-	_inviteOnly(false),
-	_topicRestricted(false),
-	_hasKey(false),
-	_userLimit(-1),
-	_creationTime(std::time(NULL)),
-	_topicSetTime(std::time(NULL)),
-	_name(name),
-	_key(""),
-	_topic(""),
-	_topicSetter(""),
-	_server(server) {}
-
-bool Channel::addMember(User* u)
-{
-    std::pair<std::set<User*>::iterator,bool> result = _members.insert(u);
-    return (result.second);
-}
-
-void Channel::removeMember(User* user)
-{
-    _members.erase(user);
-    _operators.erase(user);
-	std::map<time_t, User *>::iterator it = _membersJoinOrder.begin();
-	while (it != _membersJoinOrder.end())
-	{
-		if (it->second == user)
-		{
-			_membersJoinOrder.erase(it->first);
-			break;
-		}
-	}
-	if (_operators.empty() && !(_membersJoinOrder.empty()))
-	{
-		User *newOperator = _membersJoinOrder.begin()->second;
-		_operators.insert(newOperator);
-		broadcast(RPL_SERVER_MODE(_name,  "+o", newOperator->getNick()), user);
-	}
-	
-}
-
-
-void Channel::addOperator(User* u)
-{
-	_members.insert(u);
-	_operators.insert(u);
-	_membersJoinOrder[std::time(NULL)] = u;
-}
-void Channel::removeOperator(User* user) { _operators.erase(user); }
-
-
-std::string	Channel::getNickList() const
-{
-	std::string nickList;
-	for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
-	{
-		if (!nickList.empty())
-			nickList += " ";
-		if (isOperator(*it))
-			nickList += "@";
-		nickList += (*it)->getNick();
-	}
-	return (nickList);
-}
-
-
-void	Channel::sendWelcomeInfo(User *user)
-{
-	// envoi du topic du channel
-	if (_topic.empty())
-		Server::sendToUser(user->getSocketFd(), RPL_NOTOPIC(user->getNick(), _name));
-	else
-		Server::sendToUser(user->getSocketFd(), RPL_TOPIC(user->getNick(), _name, _topic));
-
-	// envoi de la liste des nick des users du channel (NAMES)
-	Server::sendToUser(user->getSocketFd(), RPL_NAMREPLY(user->getNick(), "=", _name, getNickList()));
-	Server::sendToUser(user->getSocketFd(), RPL_ENDOFNAMES(user->getNick(), _name));
-}
-
-bool Channel::userJoin(User *user, std::string *password)
-{  
-    if (!canUserJoin(user, password))
-			return (false);
-		
-	if (_inviteOnly)
-		rmFromInviteList(user);
-	if (!addMember(user))
-		return (Server::sendToUser(user->getSocketFd(), ERR_ALREADYJOINED(user->getNick(), _name)), false);
-	_membersJoinOrder[std::time(NULL)] = user;
-	return (true);
-}
-
-bool	Channel::canUserJoin(User *user, const std::string *password)
-{
-	if (_hasKey)
-	{
-		if (!password)
-			return (Server::sendToUser(user->getSocketFd(), ERR_NEEDMOREPARAMS(user->getNick(), "JOIN")), false);
-		std::string invalidKeyReason = Server::isValidKey(*password);
-		if (!invalidKeyReason.empty())
-			return (Server::sendToUser(user->getSocketFd(), ERR_INVALIDKEY(user->getNick(), _name, invalidKeyReason)), false);
-		if (*password != _key)
-			return (Server::sendToUser(user->getSocketFd(), ERR_BADCHANNELKEY(user->getNick(), _name)), false);
-	}
-	if (_inviteOnly && !userOnInviteList(user))
-	{
-		
-		return (Server::sendToUser(user->getSocketFd(), ERR_INVITEONLYCHAN(user->getNick(), _name)), false);
-	}
-	if ((_userLimit >= 0) && (_userLimit <= getSize()))
-		return (Server::sendToUser(user->getSocketFd(), ERR_CHANNELISFULL(user->getNick(), _name)), false);
-	return (true);
-}
-
-
-
-void Channel::broadcast(const std::string& message, User* except) const
-{
-    for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
-    {
-        User* u = *it;
-        if (u == except)
-            continue;
-        // envoie le message à chaque membre (sauf « except »)
-        Server::sendToUser(u->getSocketFd(), message);
-    }
-} 
-
-
-void Channel::printOperators() const
-{
-    std::cout << "Operators on channel " << _name << " (“ operators.size() = " << _operators.size() << "):\n";
-    for (std::set<User*>::const_iterator it = _operators.begin(); it != _operators.end(); ++it)
-        std::cout << "  - " << (*it)->getNick() << "\n";
-}
-
-void Channel::printMembers() const
-{
-    std::cout << "Members on channel " << _name << " (“ _members.size() = " << _members.size() << "):\n";
-    for (std::set<User*>::const_iterator it = _members.begin(); it != _members.end(); ++it)
-        std::cout << "  - " << (*it)->getNick() << "\n";
-}
-
-void Channel::printInviteList() const
-{
-    std::cout << "Invite users on channel " << _name << " (“ _inviteList.size() = " << _inviteList.size() << "):\n";
-    for (std::set<User*>::const_iterator it = _inviteList.begin(); it != _inviteList.end(); ++it)
-        std::cout << "  - " << (*it)->getNick() << "\n";
-}
-
-// MODE changes
-
 
 // Set/remove Invite-only channel -+i
 void	Channel::mode_invite(bool sign, std::string &userPrefix)
@@ -250,8 +85,6 @@ void	Channel::mode_userLimit(bool sign, User *user, std::string *limit)
 	}
 }
 
-
-
 void	Channel::applyMode(char mode, bool sign, User *user, IRCMessage &msg)
 {
 	std::string userPrefix = user->getFullNameMask();
@@ -310,7 +143,6 @@ void	Channel::applyMode(char mode, bool sign, User *user, IRCMessage &msg)
 	else
 		Server::sendToUser(user->getSocketFd(), ERR_UMODEUNKNOWNFLAG(user->getNick()));
 }
-
 
 // waits for any arg that starts with + or - and check all char inside to see if they are a known mode : ktoli
 // 	-> if a mode is known, search for the first arg inside the params if needed (kol), and rm this arg from other modes args
